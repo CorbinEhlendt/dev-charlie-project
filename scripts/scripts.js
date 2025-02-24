bd7ef0fa66bf00458af2a5842e1ac89f93b51345
Corbin Ehlendt <cehlendt@una.edu>
2025-02-24 16:34:54 -0600

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,11 +1,14 @@
 $( document ).ready(function() {
     console.log( "hey cumbie, don't click on the octocat" );
 
-    $('#secretCat').click(
+    $('#secretCat').on("click",function(){
+        window.open('https://www.youtube.com/watch?v=6_hl8AB7Uf0','_blank');
 
-        function(){
-            $(this).css("background-color", "yellow");
-            $('https://www.youtube.com/watch?v=6_hl8AB7Uf0').click(function(){ MyFunction(); return false; });
+        // function(){
+        //     $(this).css("background-color", "yellow");
+        //     $("#secretCat").on("click",function(){
+        //          window.open('www.yourdomain.com','_blank');
+            // $('https://www.youtube.com/watch?v=6_hl8AB7Uf0').click(function(){ MyFunction(); return false; });
             // alert('never use alert');
             }
     ); 
