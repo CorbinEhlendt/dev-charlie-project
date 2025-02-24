64ada0b999155072e5fb92a4dab822bf08553eab
Corbin Ehlendt <cehlendt@una.edu>
2025-02-24 13:23:12 -0600

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,12 @@
+$( document ).ready(function() {
+    console.log( "hey cumbie, don't click on the seal" );
 
+    $('#secretSeal').hover(
+
+        function(){
+            $(this).css("background-color", "yellow");
+            alert('never use alert');
+            }
+    ); 
+    
+});
